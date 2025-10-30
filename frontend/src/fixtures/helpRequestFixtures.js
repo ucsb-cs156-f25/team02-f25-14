75e8aea72ec0b15a529e8625ecd536cb4c53d82e
Fernando Velazquez <fvelazquez@ucsb.edu>
2025-10-30 15:36:00 -0700

    fv - add fixtures for HelpRequest table

---
+++ frontend/src/fixtures/helpRequestFixtures.js
@@ -0,0 +1,42 @@
+const helpRequestFixtures = {
+    oneHelpRequest: {
+    id: 1,
+    requesterEmail: "student1@ucsb.edu",
+    teamId: "team02",
+    tableOrBreakoutRoom: "5",
+    requestTime: "2025-10-29T12:30:00",
+    explanation: "Need help with debugging a Java Spring issue",
+    solved: false
+},
+threeHelpRequests: [
+    {
+    id: 1,
+    requesterEmail: "student1@ucsb.edu",
+    teamId: "team02",
+    tableOrBreakoutRoom: "5",
+    requestTime: "2025-10-29T12:30:00",
+    explanation: "Need help with debugging a Java Spring issue",
+    solved: false
+    },
+    {
+    id: 2,
+    requesterEmail: "student2@ucsb.edu",
+    teamId: "team03",
+    tableOrBreakoutRoom: "3",
+    requestTime: "2025-10-29T14:00:00",
+    explanation: "Having trouble with PostgreSQL migrations",
+    solved: true
+    },
+    {
+    id: 3,
+    requesterEmail: "student3@ucsb.edu",
+    teamId: "team04",
+    tableOrBreakoutRoom: "1",
+    requestTime: "2025-10-30T09:45:00",
+    explanation: "Need help understanding CRUD controller setup",
+    solved: false
+    }
+]
+};
+
+export { helpRequestFixtures };
