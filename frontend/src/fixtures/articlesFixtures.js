ff531c1141e153f9a29769c935a565509cd4ddc3
keyizong27 <keyizong@ucsb.edu>
2025-11-02 10:57:08 -0800

    Create articlesFixtures.js

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,43 @@
+const restaurantFixtures = {
+  oneRestaurant: [
+    {
+      id: 1,
+      title: "The Habit",
+      url: "https://www.habitburger.com/",
+      explanation: "A classic burger joint with a variety of tasty burgers and sides.",
+      email: "habit@gmail.com",
+      dateAdded: "2023-10-01",
+    },
+  ],
+
+  threeRestaurants: [
+    {
+      id: 2,
+      title: "In-N-Out Burger",
+      url: "https://www.in-n-out.com/",
+      explanation: "A classic burger joint with a variety of tasty burgers and sides.",
+      email: "innout@gmail.com",
+      dateAdded: "2023-10-02",
+    },
+
+    {
+      id: 3,
+      title: "MacDonald's",
+      url: "https://www.mcdonalds.com/",
+      explanation: "A classic burger joint with a variety of tasty burgers and sides.",
+      email: "macdonalds@gmail.com",
+      dateAdded: "2023-10-03",
+    },
+
+    {
+      id: 4,
+      title: "KFC",
+      url: "https://www.kfc.com/",
+      explanation: "A classic burger joint with a variety of tasty burgers and sides.",
+      email: "kfc@gmail.com",
+      dateAdded: "2023-10-04",
+    },
+  ],
+};
+
+export { restaurantFixtures };
